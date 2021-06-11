961aac4dd11578553692a70d6f3cc4846d348cf8
chenyun <yun160308@gmail.com>
2021-06-12 00:30:01 +0800

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import styles from './index.less';
+import { Input } from 'antd';
+import { Component } from 'react';
+
+class IndexPage extends Component{
+
+  onPressEnter = (e) => {
+    console.log(e.target.value);
+  }
+
+  render() {
+    return (
+      <div className={styles.searchDiv}>
+        <div className={styles.titleDiv}>SOUPU</div>
+        <Input className={styles.searchInput} placeholder='Basic usage' onPressEnter={this.onPressEnter}/>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
